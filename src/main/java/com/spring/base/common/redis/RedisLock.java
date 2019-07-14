dada4a9cfac5511b8ecb8793ebd6318a97f6100f
lihaichao <haichao.li@lechun.cc>
2019-07-14 22:35:20 +0800

    添加分布式锁

--- src/main/java/com/spring/base/common/redis/RedisLock.java
+++ src/main/java/com/spring/base/common/redis/RedisLock.java
@@ -7,22 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 public class RedisLock {
 	private static final Logger log = (Logger) LoggerFactory.getLogger(RedisLock.class);	 
-    private  Long timeout=60*1000L;
+    private  Long timeout=60L;
     private  String redisLock = "RedisLock";	
 	@Autowired
 	private RedisTemplate<String, String> redisTemplate;
 	 private void lock(String key, Method method) {
 	    long startTime=System.currentTimeMillis();
-	    try {
-	        String value = startTime+"";
-	        this.tryLock(redisLock+key, value,method);
+	    try {    	
+	        this.tryLock(redisLock+key,method);
 	        long endTime=System.currentTimeMillis();
 	        log.info("【redis分布式锁 ，运行 耗时 ："+(endTime-startTime)+" 毫秒】");
 	    }catch (Exception e){
             log.error("【redis分布式锁 锁内运行程序异常】",e);
         }
 	}
-	private void tryLock(String key, String value, Method method) {
+	private void tryLock(String key, Method method) {
+		/**
+    	 * 确保value是唯一的
+    	 */
+		String value=redisTemplate.opsForValue().increment(redisLock+"add"+key)+"";
 		try {
 			Boolean lock = redisTemplate.opsForValue().setIfAbsent(key, value, timeout, TimeUnit.SECONDS);
 			if (lock) {
@@ -31,7 +34,7 @@ public class RedisLock {
 	            this.unlock(key,value);
 			}else {
 				TimeUnit.MILLISECONDS.sleep(1);
-				tryLock( key,  value,method);
+				tryLock( key, method);
 			}
 		}catch (Exception e) {
 			log.error("【redis分布式锁  tryLock】加锁异常, {非分布式执行，请尽快查找问题}", e);
